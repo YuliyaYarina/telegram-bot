118ed6d64f24c7cab9074d6aeb072a9400c31dc5
ADMIN <Yulechka140200@gmail.com>
2024-03-27 22:24:43 +0500

    Telegram bot

--- src/main/java/pro/sky/telegrambot/model/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -1,5 +1,84 @@
 package pro.sky.telegrambot.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
 public class NotificationTask {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long chatId;
+    private String message;
+    private LocalDateTime notificationDateTime;
+
+    public NotificationTask(Long charId, String message, LocalDateTime notificationDateTime) {
+        this.chatId = charId;
+        this.message = message;
+        this.notificationDateTime = notificationDateTime;
+    }
+
+    public NotificationTask() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getNotificationDateTime() {
+        return notificationDateTime;
+    }
+
+    public void setNotificationDateTime(LocalDateTime notificationDateTime) {
+        this.notificationDateTime = notificationDateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotificationTask that = (NotificationTask) o;
+        return Objects.equals(id, that.id) && Objects.equals(chatId, that.chatId) && Objects.equals(message, that.message) && Objects.equals(notificationDateTime, that.notificationDateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, chatId, message, notificationDateTime);
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationTask{" +
+                "id=" + id +
+                ", charId=" + chatId +
+                ", message='" + message + '\'' +
+                ", notificationDateTime=" + notificationDateTime +
+                '}';
+    }
 }
